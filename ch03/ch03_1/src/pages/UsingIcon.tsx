1e2c309b0d908dcdc2a2bd4f65b1471d3f03245b
KwangYeolLee <orbit10@naver.com>
2025-09-10 22:58:55 +0900

    ch03_01_end

--- ch03/ch03_1/src/pages/UsingIcon.tsx
+++ ch03/ch03_1/src/pages/UsingIcon.tsx
@@ -1,3 +1,11 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>;
+import { Icon } from "../components";
+
+export default function UsingIcon() {
+  return (
+    <div>
+      <h3>UsingIcon</h3>
+      <Icon name="home" className="text-blue" />
+      <Icon name="check_circle_outline" className="text-red" />
+    </div>
+  );
 }
