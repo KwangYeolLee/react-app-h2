286967162a06dfb88528eb99b6958fa05cce92f8
KwangYeolLee <orbit10@naver.com>
2025-09-22 00:23:55 +0900

    ch03_04_end

--- ch03/ch03_4/src/pages/DirectionTest.tsx
+++ ch03/ch03_4/src/pages/DirectionTest.tsx
@@ -1,9 +1,34 @@
-import { Title } from "../components";
-export default function CopyMe() {
+import { Div, Title, Subtitle } from "../components";
+import * as D from "../data";
+export default function DirectionTest() {
+  const boxes = D.range(1, 10).map((number) => {
+    return (
+      <p key={number} className="border-2 border-blue-300 p-1 mt-1 ml-1">
+        {number}
+      </p>
+    );
+  });
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <Title>DirectionTest</Title>
+      <Div className="flex flex-row mt-4">
+        <Div>
+          <Subtitle>flex-row</Subtitle>
+          <Div className="flex flex-row p-4">{boxes}</Div>
+        </Div>
+        <Div className="mr-2">
+          <Subtitle>flex-row-reverse</Subtitle>
+          <Div className="flex flex-row-reverse">{boxes}</Div>
+        </Div>
+        <Div className="mr-2">
+          <Subtitle>flex-col</Subtitle>
+          <Div className="flex flex-col">{boxes}</Div>
+        </Div>
+        <Div className="mr-2">
+          <Subtitle>flex-col-reverse</Subtitle>
+          <Div className="flex flex-col-reverse">{boxes}</Div>
+        </Div>
+      </Div>
     </section>
   );
 }
