b127571df8defa1442e373900db19577276f467d
KwangYeolLee <orbit10@naver.com>
2025-09-23 23:59:59 +0900

    ch04_01_end

--- ch04/ch04_1/src/App.tsx
+++ ch04/ch04_1/src/App.tsx
@@ -1,7 +1,7 @@
+import { useClock } from "./hooks";
 import Clock from "./pages/Clock";
-
 function App() {
-  const today = new Date();
+  const today = useClock();
   return <Clock today={today} />;
 }
 export default App;
