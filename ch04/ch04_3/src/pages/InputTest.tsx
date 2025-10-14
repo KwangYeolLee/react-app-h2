2b5e7c8a2b589396489117222c10394621f58a92
2Team <orbit10@naver.com>
2025-10-14 18:10:25 +0900

    ch04_03_end

--- ch04/ch04_3/src/pages/InputTest.tsx
+++ ch04/ch04_3/src/pages/InputTest.tsx
@@ -1,9 +1,38 @@
+import type { ChangeEvent } from "react";
+import { useCallback, useState } from "react";
 import { Title } from "../components";
-export default function CopyMe() {
+import { useToggle } from "../hooks";
+
+export default function InputTest() {
+  const [value, setValue] = useState<string>("");
+  const [checked, toggleChecked] = useToggle(false);
+  const onChangeValue = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setValue((value) => e.target.value);
+  }, []);
+  const onChangeChecked = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      toggleChecked();
+    },
+    [toggleChecked]
+  );
+
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <Title>InputTest</Title>
+      <div className="flex justify-center items-center p-4 mt-4">
+        <input
+          className="input input-primary"
+          type="text"
+          value={value}
+          onChange={onChangeValue}
+        />
+        <input
+          className="checkbox checkbox-primary"
+          type="checkbox"
+          checked={checked}
+          onChange={onChangeChecked}
+        />
+      </div>
     </section>
   );
 }
