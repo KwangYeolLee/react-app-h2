3d55438188915b6fc1ca36ee00d3b899982d00eb
2Team <orbit10@naver.com>
2025-11-10 18:04:08 +0900

    ch04_05_end

--- ch04/ch04_5/src/pages/ClickTest.tsx
+++ ch04/ch04_5/src/pages/ClickTest.tsx
@@ -1,9 +1,16 @@
-import { Title } from "../components";
-export default function CopyMe() {
+import { useRef, useCallback } from "react";
+export default function ClickTest() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const onClick = useCallback(() => inputRef.current?.click(), []);
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <h2 className="text-5xl font-bold text-center">ClickTest</h2>
+      <div className="flex items-center justify-center mt-4">
+        <button className="mr-4 btn btn-primary" onClick={onClick}>
+          ClickMe
+        </button>
+        <input ref={inputRef} type="file" accept="image/*" />
+      </div>
     </section>
   );
 }
