d11f99871b088d12415ffda25026bcdfe4a743a9
2Team <orbit10@naver.com>
2025-11-11 11:10:07 +0900

    ch05_01_start

---
+++ ch05/ch05_1/src/App.tsx
@@ -0,0 +1,10 @@
+import ReduxClock from "./pages/ReduxClock";
+import UseReducerClock from "./pages/UseReducerClock";
+export default function App() {
+  return (
+    <main className="p-8">
+      <UseReducerClock />
+      <ReduxClock />
+    </main>
+  );
+}
